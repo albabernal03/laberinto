2308c4053a7cc1178f1181f5790e85ba32cedcd9
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-08 20:51:42 +0100

    despues de miles de intentos he visto que el el fallo estaba en la tabulación del return laberinto

--- laberinto.py
+++ laberinto.py
@@ -8,28 +8,26 @@ salida = ((4,4),(4,4))
 
 #En primer lugar definimos las dimensiones del laberinto y el recorrido
 
+
+
 def laberinto(dimension,muros):
-  laberinto = []
+    laberinto = []
+    for i in range(dimension):
+        fila = []
+        for j in range(dimension):
+            if tuple ([i,j]) in muros:
+                fila.append('X')
+            elif tuple ([i,j]) in entrada:
+                fila.append('E')
+            elif tuple ([i,j]) in salida:
+                fila.append('S')
+            else:
+                fila.append(' ')
+        laberinto.append(fila)
+    return laberinto
+            
 
-  for i in range(dimension): 
-      fila = []
-
-      for j in range(dimension):
-
-          if tuple ([i,j]) in muros:
-              fila.append('X')
-
-          elif tuple ([i,j]) in entrada:
-              fila.append('E')
-
-          elif tuple ([i,j]) in salida:
-              fila.append('S')
-
-          else:
-              fila.append(' ')
-      laberinto.append(fila)
-      return laberinto 
-
+          
           
 imagenlaberinto = laberinto(6,muros)
 
